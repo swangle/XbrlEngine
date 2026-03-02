86b9ee87fd93ad4e67c39462128d788c0b0415cc
swangle <65751979+swangle@users.noreply.github.com>
2026-03-02 19:33:53 +0530

    Create Models/Core/Concept.cs

---
+++ src/XbrlEngine/Models/Core/Concept.cs
@@ -0,0 +1,11 @@
+// Concept.cs
+
+namespace Models.Core
+{
+    public class Concept
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+    }
+}
